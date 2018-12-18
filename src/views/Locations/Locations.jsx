399869c9ce9cb41b8dbb58b3058661034b2e7776
aramkoukia <aramkoukia@gmail.com>
2018-12-17 18:58:21 -0800

    added api call

--- src/views/Locations/Locations.jsx
+++ src/views/Locations/Locations.jsx
@@ -10,75 +10,87 @@ import CardBody from "components/Card/CardBody.jsx";
 import Button from "components/CustomButtons/Button.jsx";
 
 import MUIDataTable from "mui-datatables";
-
 import AddLocation from "views/Locations/AddLocation";
 
-const styles = {
-  cardCategoryWhite: {
-    "&,& a,& a:hover,& a:focus": {
-      color: "rgba(255,255,255,.62)",
-      margin: "0",
-      fontSize: "14px",
-      marginTop: "0",
-      marginBottom: "0"
-    },
-    "& a,& a:hover,& a:focus": {
-      color: "#FFFFFF"
-    }
-  },
-  cardTitleWhite: {
-    color: "#FFFFFF",
-    marginTop: "0px",
-    minHeight: "auto",
-    fontWeight: "300",
-    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
-    marginBottom: "3px",
-    textDecoration: "none",
-    "& small": {
-      color: "#777",
-      fontSize: "65%",
-      fontWeight: "400",
-      lineHeight: "1"
-    }
+export default class Locations extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { locations: [] };
   }
-};
 
-function Locations(props) {
-  const { classes } = props;
-  const columns = ["Name", "Address"];
+  componentDidMount() {
+    this.UserList();
+  }
 
-  const data = [
-    ["Vancouver", "adress ..."],
-    ["Abbotsforf", "adress ..."],
-  ];
+  UserList() {
+    fetch("https://lightsandpartsapi.azurewebsites.net/api/locations")
+      .then(response => response.json())
+      .then(data => this.setState({ locations: data }));
+  }
 
-  const options = {
-    filterType: 'checkbox',
-  };
+  render() {
+    const styles = {
+      cardCategoryWhite: {
+        "&,& a,& a:hover,& a:focus": {
+          color: "rgba(255,255,255,.62)",
+          margin: "0",
+          fontSize: "14px",
+          marginTop: "0",
+          marginBottom: "0"
+        },
+        "& a,& a:hover,& a:focus": {
+          color: "#FFFFFF"
+        }
+      },
+      cardTitleWhite: {
+        color: "#FFFFFF",
+        marginTop: "0px",
+        minHeight: "auto",
+        fontWeight: "300",
+        fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+        marginBottom: "3px",
+        textDecoration: "none",
+        "& small": {
+          color: "#777",
+          fontSize: "65%",
+          fontWeight: "400",
+          lineHeight: "1"
+        }
+      }
+    };
 
-  return (
-    <div>
-      <Button color="primary">Add Location</Button>
-      <AddLocation />
-      <GridContainer>
-      <GridItem xs={12} sm={12} md={12}>
-        <Card>
-          <CardHeader color="primary">
-            <h4 className={classes.cardTitleWhite}>Locations List</h4>
-          </CardHeader>
-          <CardBody>
-              <MUIDataTable
-                // title={"Employee List"}
-                data={data}
-                columns={columns}
-                options={options}
-              />
-          </CardBody>
-        </Card>
-      </GridItem>
-    </GridContainer>
-    </div>
-  );
+    const columns = ["locationId", "locationName", "locationAddress"];
+    const options = {
+      filterType: "checkbox",
+    };
+
+    const { locations } = this.state;
+
+    return (
+      <div>
+        <Button color="primary">Add Location</Button>
+        <AddLocation />
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={12}>
+            <Card>
+              <CardHeader color="primary">
+                <h4 className={styles.cardTitleWhite}>Locations List</h4>
+              </CardHeader>
+              <CardBody>
+                <MUIDataTable
+                  // title={"Employee List"}
+                  data={locations}
+                  columns={columns}
+                  options={options}
+                />
+              </CardBody>
+            </Card>
+          </GridItem>
+        </GridContainer>
+      </div>
+    );
+  }
 }
 
-export default withStyles(styles)(Locations);
+// export default withStyles(styles)(Locations);
