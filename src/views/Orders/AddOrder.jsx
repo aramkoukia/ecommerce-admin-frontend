43083aa9b400e3d19742690fa58057d09dc1b966
aramkoukia <aramkoukia@gmail.com>
2018-12-21 17:17:12 -0800

    added auto complete

--- src/views/Orders/AddOrder.jsx
+++ src/views/Orders/AddOrder.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 // @material-ui/core components
-import withStyles from "@material-ui/core/styles/withStyles";
-import InputLabel from "@material-ui/core/InputLabel";
+import { withStyles } from "@material-ui/core/styles";
 // core components
 import GridItem from "components/Grid/GridItem.jsx";
 import GridContainer from "components/Grid/GridContainer.jsx";
@@ -9,13 +8,18 @@ import CustomInput from "components/CustomInput/CustomInput.jsx";
 import Button from "components/CustomButtons/Button.jsx";
 import Card from "components/Card/Card.jsx";
 import CardHeader from "components/Card/CardHeader.jsx";
-import CardAvatar from "components/Card/CardAvatar.jsx";
 import CardBody from "components/Card/CardBody.jsx";
 import CardFooter from "components/Card/CardFooter.jsx";
-
-import avatar from "assets/img/faces/marc.jpg";
-
+import Popper from "@material-ui/core/Popper";
+import Autosuggest from "react-autosuggest";
 import Search from "@material-ui/icons/Search";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
+import MenuItem from "@material-ui/core/MenuItem";
+import match from "autosuggest-highlight/match";
+import parse from "autosuggest-highlight/parse";
+import deburr from "lodash/deburr";
+import PropTypes from "prop-types";
 
 const styles = {
   cardCategoryWhite: {
@@ -36,79 +40,235 @@ const styles = {
   }
 };
 
-const columns = ["Order Date", "Total", "PO Number", "Status", "Customer"];
+function renderInputComponent(inputProps) {
+  const { classes, inputRef = () => {}, ref, ...other } = inputProps;
 
-const data = [
-  ["11/12/2019", "120.00", "2123", "Unpaid", "Aram Koukia"],
-  ["11/12/2019", "150.00", "2344", "Draft", "Test"],
-  ["11/12/2019", "10.00", "2344", "On Hold", ""],
-  ["11/12/2019", "150.00", "234", "Completed", ""],
-];
-
-const options = {
-  filterType: 'checkbox',
-};
-
-function AddOrder(props) {
-  const { classes } = props;
   return (
-    <div>
-      <GridContainer>
-        <GridItem xs={12} sm={12} md={8}>
-          <Card>
-            <CardHeader color="primary">
-              <h4 className={classes.cardTitleWhite}>New Order</h4>
-            </CardHeader>
-            <CardBody>
-              <GridContainer>
-                <GridItem xs={12} sm={12} md={5}>
-                  <CustomInput
-                    labelText="Search Product"
-                    id="search_product"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                  />
-                </GridItem>
-              </GridContainer>
-              <GridContainer>
-                <GridItem xs={12} sm={12} md={12}>
-
-                </GridItem>
-              </GridContainer>
-            </CardBody>
-            <CardFooter>
-              <Button color="primary">Complete</Button>
-              <Button color="secondary">On Hold</Button>
-              <Button color="secondary">Draft</Button>
-              <Button color="secondary">Account</Button>
-            </CardFooter>
-          </Card>
-        </GridItem>
-        <GridItem xs={12} sm={12} md={4}>
-          <Card profile>
-            <CardBody profile>
-              <CustomInput
-                formControlProps={{
-                  className: classes.margin + " " + classes.search
-                }}
-                inputProps={{
-                  placeholder: "Search",
-                  inputProps: {
-                    "aria-label": "Search"
-                  }
-                }}
-              />
-              <Button color="white" aria-label="edit" justIcon round>
-                <Search />
-              </Button>
-              <Button color="primary">New Customer</Button>
-            </CardBody>
-          </Card>
-        </GridItem>
-      </GridContainer>
-    </div>
+    <TextField
+      fullWidth
+      InputProps={{
+        inputRef: node => {
+          ref(node);
+          inputRef(node);
+        },
+        classes: {
+          input: classes.input
+        }
+      }}
+      {...other}
+    />
   );
 }
 
+const suggestions = [
+  { label: "Afghanistan" },
+  { label: "Aland Islands" },
+  { label: "Albania" },
+  { label: "Algeria" },
+  { label: "American Samoa" },
+  { label: "Andorra" },
+  { label: "Angola" },
+  { label: "Anguilla" },
+  { label: "Antarctica" },
+  { label: "Antigua and Barbuda" },
+  { label: "Argentina" },
+  { label: "Armenia" },
+  { label: "Aruba" },
+  { label: "Australia" },
+  { label: "Austria" },
+  { label: "Azerbaijan" },
+  { label: "Bahamas" },
+  { label: "Bahrain" },
+  { label: "Bangladesh" },
+  { label: "Barbados" },
+  { label: "Belarus" },
+  { label: "Belgium" },
+  { label: "Belize" },
+  { label: "Benin" },
+  { label: "Bermuda" },
+  { label: "Bhutan" },
+  { label: "Bolivia, Plurinational State of" },
+  { label: "Bonaire, Sint Eustatius and Saba" },
+  { label: "Bosnia and Herzegovina" },
+  { label: "Botswana" },
+  { label: "Bouvet Island" },
+  { label: "Brazil" },
+  { label: "British Indian Ocean Territory" },
+  { label: "Brunei Darussalam" }
+];
+
+function getSuggestions(value) {
+  const inputValue = deburr(value.trim()).toLowerCase();
+  const inputLength = inputValue.length;
+  let count = 0;
+
+  return inputLength === 0
+    ? []
+    : suggestions.filter(suggestion => {
+        const keep =
+          count < 5 &&
+          suggestion.label.slice(0, inputLength).toLowerCase() === inputValue;
+
+        if (keep) {
+          count += 1;
+        }
+
+        return keep;
+      });
+}
+
+function getSuggestionValue(suggestion) {
+  return suggestion.label;
+}
+
+function renderSuggestion(suggestion, { query, isHighlighted }) {
+  const matches = match(suggestion.label, query);
+  const parts = parse(suggestion.label, matches);
+
+  return (
+    <MenuItem selected={isHighlighted} component="div">
+      <div>
+        {parts.map((part, index) => {
+          return part.highlight ? (
+            <span key={String(index)} style={{ fontWeight: 500 }}>
+              {part.text}
+            </span>
+          ) : (
+            <strong key={String(index)} style={{ fontWeight: 300 }}>
+              {part.text}
+            </strong>
+          );
+        })}
+      </div>
+    </MenuItem>
+  );
+}
+
+export class AddOrder extends React.Component {
+  state = {
+    single: "",
+    popper: "",
+    suggestions: []
+  };
+
+  handleSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: getSuggestions(value)
+    });
+  };
+
+  handleSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
+
+  handleChange = name => (event, { newValue }) => {
+    this.setState({
+      [name]: newValue
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    const autosuggestProps = {
+      renderInputComponent,
+      suggestions: this.state.suggestions,
+      onSuggestionsFetchRequested: this.handleSuggestionsFetchRequested,
+      onSuggestionsClearRequested: this.handleSuggestionsClearRequested,
+      getSuggestionValue,
+      renderSuggestion
+    };
+
+    return (
+      <div>
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={8}>
+            <Card>
+              <CardHeader color="primary">
+                <h4 className={classes.cardTitleWhite}>New Order</h4>
+              </CardHeader>
+              <CardBody>
+                <GridContainer>
+                  <GridItem xs={12} sm={12} md={5}>
+                    <Autosuggest
+                      {...autosuggestProps}
+                      inputProps={{
+                        classes,
+                        label: "Label",
+                        placeholder: "Search Products",
+                        value: this.state.popper,
+                        onChange: this.handleChange("popper"),
+                        inputRef: node => {
+                          this.popperNode = node;
+                        },
+                        InputLabelProps: {
+                          shrink: true
+                        }
+                      }}
+                      theme={{
+                        suggestionsList: classes.suggestionsList,
+                        suggestion: classes.suggestion
+                      }}
+                      renderSuggestionsContainer={options => (
+                        <Popper
+                          anchorEl={this.popperNode}
+                          open={Boolean(options.children)}
+                        >
+                          <Paper
+                            square
+                            {...options.containerProps}
+                            style={{
+                              width: this.popperNode
+                                ? this.popperNode.clientWidth
+                                : null
+                            }}
+                          >
+                            {options.children}
+                          </Paper>
+                        </Popper>
+                      )}
+                    />
+                    <CustomInput
+                      labelText="Search Product"
+                      id="search_product"
+                      formControlProps={{
+                        fullWidth: true
+                      }}
+                    />
+                  </GridItem>
+                </GridContainer>
+                <GridContainer>
+                  <GridItem xs={12} sm={12} md={12} />
+                </GridContainer>
+              </CardBody>
+              <CardFooter>
+                <Button color="primary">Complete</Button>
+                <Button color="secondary">On Hold</Button>
+                <Button color="secondary">Draft</Button>
+                <Button color="secondary">Account</Button>
+              </CardFooter>
+            </Card>
+          </GridItem>
+          <GridItem xs={12} sm={12} md={4}>
+            <Card profile>
+              <CardBody profile>
+                <Button color="white" aria-label="edit" justIcon round>
+                  <Search />
+                </Button>
+                <Button color="primary">New Customer</Button>
+              </CardBody>
+            </Card>
+          </GridItem>
+        </GridContainer>
+      </div>
+    );
+  }
+}
+
 export default withStyles(styles)(AddOrder);
+
+AddOrder.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
