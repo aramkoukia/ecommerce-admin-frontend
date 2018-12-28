2b81446db2a7b053c849546ac5fcf31d444ebfdc
Aram Koukia <aramkoukia@gmail.com>
2018-12-28 11:57:43 -0800

    added company name

--- src/views/Customers/Customers.jsx
+++ src/views/Customers/Customers.jsx
@@ -1,17 +1,11 @@
-import React from "react";
-// @material-ui/core components
-// import withStyles from "@material-ui/core/styles/withStyles";
-// core components
-import GridItem from "components/Grid/GridItem.jsx";
-import GridContainer from "components/Grid/GridContainer.jsx";
-import Card from "components/Card/Card.jsx";
-import CardHeader from "components/Card/CardHeader.jsx";
-import CardBody from "components/Card/CardBody.jsx";
-// import Button from "components/CustomButtons/Button.jsx";
-
-import MUIDataTable from "mui-datatables";
-import CustomerService from "../../services/CustomerService";
-// import AddLocation from "views/Locations/AddLocation";
+import React from 'react';
+import MUIDataTable from 'mui-datatables';
+import GridItem from '../../components/Grid/GridItem';
+import GridContainer from '../../components/Grid/GridContainer';
+import CardHeader from '../../components/Card/CardHeader';
+import CardBody from '../../components/Card/CardBody';
+import Card from '../../components/Card/Card';
+import CustomerService from '../../services/CustomerService';
 
 export default class Customers extends React.Component {
   constructor(props) {
@@ -25,53 +19,47 @@ export default class Customers extends React.Component {
   }
 
   customersList() {
-    const columns = ["customerCode", "firstName", "lastName", "email", "creditLimit", "pstNumber"];
+    const columns = ['customerCode', 'companyName', 'firstName', 'lastName', 'email', 'creditLimit', 'pstNumber'];
     CustomerService.getCustomers()
-      .then(results => {
-        return results.map(row => {
-          return columns.map(column => {
-            return row[column] || "";
-          });
-        });
-      })
+      .then(results => results.map(row => columns.map(column => row[column] || '')))
       .then(data => this.setState({ customers: data }));
   }
 
   render() {
     const styles = {
       cardCategoryWhite: {
-        "&,& a,& a:hover,& a:focus": {
-          color: "rgba(255,255,255,.62)",
-          margin: "0",
-          fontSize: "14px",
-          marginTop: "0",
-          marginBottom: "0"
+        '&,& a,& a:hover,& a:focus': {
+          color: 'rgba(255,255,255,.62)',
+          margin: '0',
+          fontSize: '14px',
+          marginTop: '0',
+          marginBottom: '0',
         },
-        "& a,& a:hover,& a:focus": {
-          color: "#FFFFFF"
-        }
+        '& a,& a:hover,& a:focus': {
+          color: '#FFFFFF',
+        },
       },
       cardTitleWhite: {
-        color: "#FFFFFF",
-        marginTop: "0px",
-        minHeight: "auto",
-        fontWeight: "300",
+        color: '#FFFFFF',
+        marginTop: '0px',
+        minHeight: 'auto',
+        fontWeight: '300',
         fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
-        marginBottom: "3px",
-        textDecoration: "none",
-        "& small": {
-          color: "#777",
-          fontSize: "65%",
-          fontWeight: "400",
-          lineHeight: "1"
-        }
-      }
+        marginBottom: '3px',
+        textDecoration: 'none',
+        '& small': {
+          color: '#777',
+          fontSize: '65%',
+          fontWeight: '400',
+          lineHeight: '1',
+        },
+      },
     };
 
-    const columns = ["Customer Code", "First Name", "Last Name", "Email", "Credit Limit ($)", "PST Number"];
+    const columns = ['Customer Code', 'Company Name', 'First Name', 'Last Name', 'Email', 'Credit Limit ($)', 'PST Number'];
 
     const options = {
-      filterType: "checkbox",
+      filterType: 'checkbox',
       // onRowClick: this.rowClicked,
       rowHover: true,
       resizableColumns: true,
@@ -86,7 +74,7 @@ export default class Customers extends React.Component {
           <GridItem xs={12} sm={12} md={12}>
             <Card>
               <CardHeader color="primary">
-                <h4 className={styles.cardTitleWhite}>Customers List</h4>
+                <div className={styles.cardTitleWhite}>Customers List</div>
               </CardHeader>
               <CardBody>
                 <MUIDataTable
@@ -102,5 +90,3 @@ export default class Customers extends React.Component {
     );
   }
 }
-
-// export default withStyles(styles)(Customers);
