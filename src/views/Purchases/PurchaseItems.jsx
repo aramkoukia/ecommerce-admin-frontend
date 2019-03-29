926f636117a005b60bb889539cf8b76717d74c09
aramkoukia <aramkoukia@gmail.com>
2019-03-29 16:31:32 -0700

    added purchase statuses

--- src/views/Purchases/PurchaseItems.jsx
+++ src/views/Purchases/PurchaseItems.jsx
@@ -5,10 +5,11 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Card from 'components/Card/Card';
-import CardHeader from 'components/Card/CardHeader';
-import CardBody from 'components/Card/CardBody';
-import Success from 'components/Typography/Success';
+import Success from '../../components/Typography/Success';
+import Button from '../../components/CustomButtons/Button';
+import Card from '../../components/Card/Card';
+import CardHeader from '../../components/Card/CardHeader';
+import CardBody from '../../components/Card/CardBody';
 
 const style = {
   cardTitleWhite: {
@@ -26,26 +27,77 @@ function ccyFormat(num) {
   return `${num.toFixed(2)} $`;
 }
 
+function markAsPaidClicked() {
+
+}
+
 function PurchaseItems(props) {
   const { classes, purchase } = props;
 
   return (
-    <Card>
-      <CardHeader color="info">
-        <div className={classes.cardTitleWhite}>Purchase Items</div>
-      </CardHeader>
-      <CardBody>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>Product</TableCell>
-              <TableCell numeric>Amount</TableCell>
-              <TableCell numeric>Unit Price</TableCell>
-              <TableCell numeric>Total Price</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {purchase.purchaseDetail.map(row => (
+    <div>
+      <Card>
+        <CardHeader color="info">
+          <div className={classes.cardTitleWhite}>Planned Items</div>
+        </CardHeader>
+        <CardBody>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Product</TableCell>
+                <TableCell numeric>Amount</TableCell>
+                <TableCell numeric>Unit Price</TableCell>
+                <TableCell numeric>Total Price</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {purchase.purchaseDetail.map(row => (
+                <TableRow key={row.productId}>
+                  <TableCell>
+                    <Button color="primary" onClick={markAsPaidClicked(row.productId)}>Paid</Button>
+                    &nbsp;
+                    &nbsp;
+                    {row.product.productCode}
+                    &nbsp;
+                    &nbsp;
+                    {row.product.productName}
+                  </TableCell>
+                  <TableCell numeric align="right">{row.amount}</TableCell>
+                  <TableCell numeric>{ccyFormat(row.unitPrice)}</TableCell>
+                  <TableCell numeric>{ccyFormat(row.totalPrice)}</TableCell>
+                </TableRow>
+              ))}
+              <TableRow>
+                <TableCell rowSpan={5} />
+                <TableCell colSpan={2}>Subtotal</TableCell>
+                <TableCell numeric>{ccyFormat(purchase.subTotal)}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell colSpan={2}><h3>Total</h3></TableCell>
+                <TableCell numeric><Success><h3>{ccyFormat(purchase.total)}</h3></Success></TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </CardBody>
+      </Card>
+      <Card>
+        <CardHeader color="info">
+          <div className={classes.cardTitleWhite}>Paid Items</div>
+        </CardHeader>
+        <CardBody>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Product</TableCell>
+                <TableCell numeric>Amount</TableCell>
+                <TableCell numeric>Unit Price</TableCell>
+                <TableCell numeric>Total Price</TableCell>
+                <TableCell>Paid Date</TableCell>
+                <TableCell>Estimated Delivery</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {/* {purchase.purchaseDetail.map(row => (
               <TableRow key={row.productId}>
                 <TableCell>{row.product.productName}</TableCell>
                 <TableCell numeric align="right">{row.amount}</TableCell>
@@ -61,11 +113,124 @@ function PurchaseItems(props) {
             <TableRow>
               <TableCell colSpan={2}><h3>Total</h3></TableCell>
               <TableCell numeric><Success><h3>{ccyFormat(purchase.total)}</h3></Success></TableCell>
+            </TableRow> */}
+            </TableBody>
+          </Table>
+        </CardBody>
+      </Card>
+      <Card>
+        <CardHeader color="info">
+          <div className={classes.cardTitleWhite}>On Delivery Items</div>
+        </CardHeader>
+        <CardBody>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Product</TableCell>
+                <TableCell numeric>Amount</TableCell>
+                <TableCell numeric>Unit Price</TableCell>
+                <TableCell numeric>Total Price</TableCell>
+                <TableCell>Estimated Delivery</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {/* {purchase.purchaseDetail.map(row => (
+              <TableRow key={row.productId}>
+                <TableCell>{row.product.productName}</TableCell>
+                <TableCell numeric align="right">{row.amount}</TableCell>
+                <TableCell numeric>{ccyFormat(row.unitPrice)}</TableCell>
+                <TableCell numeric>{ccyFormat(row.totalPrice)}</TableCell>
+              </TableRow>
+            ))}
+            <TableRow>
+              <TableCell rowSpan={5} />
+              <TableCell colSpan={2}>Subtotal</TableCell>
+              <TableCell numeric>{ccyFormat(purchase.subTotal)}</TableCell>
             </TableRow>
-          </TableBody>
-        </Table>
-      </CardBody>
-    </Card>
+            <TableRow>
+              <TableCell colSpan={2}><h3>Total</h3></TableCell>
+              <TableCell numeric><Success><h3>{ccyFormat(purchase.total)}</h3></Success></TableCell>
+            </TableRow> */}
+            </TableBody>
+          </Table>
+        </CardBody>
+      </Card>
+      <Card>
+        <CardHeader color="info">
+          <div className={classes.cardTitleWhite}>Custom Clearance Items</div>
+        </CardHeader>
+        <CardBody>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Product</TableCell>
+                <TableCell numeric>Amount</TableCell>
+                <TableCell numeric>Unit Price</TableCell>
+                <TableCell numeric>Total Price</TableCell>
+                <TableCell>Estimated Delivery</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {/* {purchase.purchaseDetail.map(row => (
+              <TableRow key={row.productId}>
+                <TableCell>{row.product.productName}</TableCell>
+                <TableCell numeric align="right">{row.amount}</TableCell>
+                <TableCell numeric>{ccyFormat(row.unitPrice)}</TableCell>
+                <TableCell numeric>{ccyFormat(row.totalPrice)}</TableCell>
+              </TableRow>
+            ))}
+            <TableRow>
+              <TableCell rowSpan={5} />
+              <TableCell colSpan={2}>Subtotal</TableCell>
+              <TableCell numeric>{ccyFormat(purchase.subTotal)}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell colSpan={2}><h3>Total</h3></TableCell>
+              <TableCell numeric><Success><h3>{ccyFormat(purchase.total)}</h3></Success></TableCell>
+            </TableRow> */}
+            </TableBody>
+          </Table>
+        </CardBody>
+      </Card>
+      <Card>
+        <CardHeader color="info">
+          <div className={classes.cardTitleWhite}>Arrived Items</div>
+        </CardHeader>
+        <CardBody>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Product</TableCell>
+                <TableCell numeric>Amount</TableCell>
+                <TableCell numeric>Unit Price</TableCell>
+                <TableCell numeric>Total Price</TableCell>
+                <TableCell>Arrived Date</TableCell>
+                <TableCell>Location</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {/* {purchase.purchaseDetail.map(row => (
+              <TableRow key={row.productId}>
+                <TableCell>{row.product.productName}</TableCell>
+                <TableCell numeric align="right">{row.amount}</TableCell>
+                <TableCell numeric>{ccyFormat(row.unitPrice)}</TableCell>
+                <TableCell numeric>{ccyFormat(row.totalPrice)}</TableCell>
+              </TableRow>
+            ))}
+            <TableRow>
+              <TableCell rowSpan={5} />
+              <TableCell colSpan={2}>Subtotal</TableCell>
+              <TableCell numeric>{ccyFormat(purchase.subTotal)}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell colSpan={2}><h3>Total</h3></TableCell>
+              <TableCell numeric><Success><h3>{ccyFormat(purchase.total)}</h3></Success></TableCell>
+            </TableRow> */}
+            </TableBody>
+          </Table>
+        </CardBody>
+      </Card>
+    </div>
   );
 }
 
