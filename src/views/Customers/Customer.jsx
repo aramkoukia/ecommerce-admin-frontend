465f549cd55ffc737c78d3f6053ceaebe3ef60fa
aramkoukia <aramkoukia@gmail.com>
2019-04-11 20:04:36 -0700

    fixing order dates

--- src/views/Customers/Customer.jsx
+++ src/views/Customers/Customer.jsx
@@ -17,7 +17,11 @@ import CustomerService from '../../services/CustomerService';
 
 function dateFormat(dateString) {
   const date = new Date(dateString);
-  return `${date.toLocaleDateString()}`;
+  const year = date.getFullYear();
+  const month = `${date.getMonth() + 1}`.padStart(2, 0);
+  const day = `${date.getDate()}`.padStart(2, 0);
+  const stringDate = [day, month, year].join('/');
+  return `${stringDate} ${date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}`;
 }
 
 export default class Customer extends React.Component {
@@ -78,9 +82,6 @@ export default class Customer extends React.Component {
     this.setState({ loading: true });
     OrderService.getCustomerOrders(customerId)
       .then(results => results.map(row => columns.map((column) => {
-        if (column === 'orderDate') {
-          return dateFormat(row[column]);
-        }
         return row[column] || '';
       })))
       .then(data => this.setState({ orders: data, loading: false }));
