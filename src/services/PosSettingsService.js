e4e5a8e7b43bc5efaf7c470c0f79ae8bbafdc344
aramkoukia <aramkoukia@gmail.com>
2019-08-24 11:27:46 -0700

    adding services

--- src/services/PosSettingsService.js
+++ src/services/PosSettingsService.js
@@ -2,4 +2,36 @@
 import RestUtilities from './RestUtilities';
 
 export default class PosSettingsService {
+  static async pairPos() {
+    try {
+      const response = await RestUtilities.get(
+        'moneris/pair',
+      );
+      return response.content;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  static async unpairPos() {
+    try {
+      const response = await RestUtilities.get(
+        'moneris/unpair',
+      );
+      return response.content;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  static async initializePos() {
+    try {
+      const response = await RestUtilities.get(
+        'moneris/initialize',
+      );
+      return response.content;
+    } catch (err) {
+      return false;
+    }
+  }
 }
