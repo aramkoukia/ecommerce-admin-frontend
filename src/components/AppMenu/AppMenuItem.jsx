f5671ddc9a79642065d18a3df0cb7ae87a261682
somayehgh <somayehghazvinian@gmail.com>
2020-10-04 12:16:55 -0700

    fixing permissions stuff

--- src/components/AppMenu/AppMenuItem.jsx
+++ src/components/AppMenu/AppMenuItem.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import classNames from 'classnames';
-// import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import {
   List,
@@ -16,7 +14,7 @@ import IconExpandMore from '@material-ui/icons/ExpandMore';
 import Auth from '../../services/Auth';
 import AppMenuItemComponent from './AppMenuItemComponent';
 
-const useStyles = makeStyles((theme) => createStyles({
+const useStyles = makeStyles(() => createStyles({
   menuItem: {
     color: 'white',
     '& .MuiListItem-root': {
@@ -46,12 +44,12 @@ const AppMenuItem = (props) => {
     permission,
     path,
     Icon,
+    needsPermission,
     items = [],
   } = props;
 
   const isExpandable = items && items.length > 0;
-  const userHasPermission = () => (path !== undefined
-                                   && Auth.userHasPermission(permission)) || path === undefined;
+  const userHasPermission = needsPermission && Auth.userHasPermission(permission);
   const isNavigation = sidebarName !== '';
   const [open, setOpen] = React.useState(false);
 
@@ -59,7 +57,7 @@ const AppMenuItem = (props) => {
     setOpen(!open);
   }
 
-  const MenuItemRoot = isNavigation && (
+  const MenuItemRoot = isNavigation && userHasPermission && (
     <AppMenuItemComponent className={classes.menuItem} link={path} onClick={handleClick}>
       <ListItem button className={classes.menuItem} onClick={handleClick}>
         {/* Display an icon if any */}
@@ -84,15 +82,25 @@ const AppMenuItem = (props) => {
     </AppMenuItemComponent>
   );
 
-  const MenuItemChildren = userHasPermission()
+  const MenuItemChildren = userHasPermission
     && isExpandable
     ? (
       <Collapse in={open} timeout="auto" unmountOnExit>
         <Divider />
         <List component="div" style={{ padding: 0, margin: 0 }}>
-          {items.map((menu, index) => (
-            <AppMenuItem {...menu} key={index} />
-          ))}
+          {items
+            .filter((item) => item.needsPermission && Auth.userHasPermission(item.permission))
+            .map((menu, index) => (
+              <AppMenuItem
+                sidebarName={menu.sidebarName}
+                permission={menu.permission}
+                path={menu.path}
+                Icon={menu.Icon}
+                items={menu.items}
+                needsPermission={menu.needsPermission}
+                key={menu.sidebarName + index}
+              />
+            ))}
         </List>
       </Collapse>
     ) : null;
@@ -111,6 +119,7 @@ AppMenuItem.propTypes = {
   history: PropTypes.object.isRequired,
   items: PropTypes.object,
   sidebarName: PropTypes.object,
+  needsPermission: PropTypes.bool,
   permission: PropTypes.object,
   path: PropTypes.object,
   Icon: PropTypes.object,
